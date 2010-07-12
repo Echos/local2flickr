89a1cf1c605c0faf9a4a9a4ae8fd57e082854449
えちょ <echos@echo-sp.com>
2010-07-12 19:35:55 +0900

    photosetに対応

--- local2flickr.rb
+++ local2flickr.rb
@@ -18,33 +18,27 @@ def checkoption
   begin 
     #コマンドラインオプション定義
     opt.on('-h','--help','USAGEを表示。')   {|v| puts opt.help;exit }
-    opt.on('-f FILE', '--file FILE' ,'アップロードするファイル')    {|v| options.file = v }
-    opt.on('-t=[TITLE]', '--title=[TITLE]', 'ファイルのタイトル。') {|v| options.title = v}
-    opt.on('-d=[DESCRIPTION]', '--DESCRIPTION=[DESCRIPTION]', 'ファイルの説明') {|v| options.description = v}
-    opt.on('-T=[TAGS]','--tags=[TAGS]', Array, "登録するタグ（複数可能） (カンマ区切り)") {|v| options.tags = v}
+    opt.on('-f FILE', '--file=FILE' ,'アップロードするファイル')    {|v| options.file = v }
+    opt.on('-P PHOTOSET', '--photoset=PHOTOSET' ,'登録するフォトセット名称')    {|v| options.photoset = v }
+    opt.on('-t TITLE', '--title=TITLE', 'ファイルのタイトル。') {|v| options.title = v}
+    opt.on('-d DESCRIPTION', '--DESCRIPTION=DESCRIPTION', 'ファイルの説明') {|v| options.description = v}
+    opt.on('-T  TAGS','--tags=TAGS', Array, "登録するタグ（複数可能） (カンマ区切り)") {|v| options.tags = v}
     opt.on('-p','--is-private', 'プライベート設定')                 {|v| options.private =v}
     opt.on('--friend', '--friends', '友達参照可')      { |v| options.friend = v }
     opt.on('--family', '家族参照可')                   { |v| options.family = v }
     #オプションのパース
     opt.parse!(ARGV)
-    p options.file
 
     if(options.file==nil)
       puts opt.help
       exit
     end
 
-    if options.tags
-      options.tags.each do |tag|
-        tag.gsub!(/^(.*) (.*)$/, '"\1 \2"')
-      end
-    end
-
     return options
   rescue
     #指定外のオプションが存在する場合はUsageを表示
     puts opt.help
-    exi
+    exit
   end
 end
 
@@ -119,14 +113,13 @@ def upload(ac_f,opts)
   ep = 'http://api.flickr.com/services/upload/'
 
   #指定したファイルが存在するか確認
-  p opts.file
   (puts "#{opts.file} はファイルではありません。";exit) if ! test ?r, opts.file
 
   file = File.new(opts.file)
   params = Hash.new
-  params["title"] = options.title if opts.title
-  params["description"] = options.description if opts.description
-  params["tags"] = options.tags.join(" ") if opts.tags
+  params["title"] = opts.title if opts.title
+  params["description"] = ops.description if opts.description
+  params["tags"] = opts.tags.join(" ") if opts.tags
   params["is_public"] = 0 if opts.private
   params["is_friend"] = 1 if opts.friend
   params["is_family"] = 1 if opts.family
@@ -139,6 +132,72 @@ def upload(ac_f,opts)
   doc = Hpricot(page.body)
   return doc.at("photoid").inner_text
 end
+
+#自分のユーザIDを取得する
+def getuserid(ac_f)
+  ep = 'http://flickr.com/services/rest/'
+  params = Hash.new
+  params["method"] = "flickr.auth.checkToken"
+  params["api_key"] = ac_f['api_key']
+  params["auth_token"] = ac_f['token']
+  params["api_sig"] = get_api_sig(params,ac_f["secret"])
+
+  agent = Mechanize.new
+  page = agent.post(ep,params)
+  doc = Hpricot(page.body)
+  return  (doc/:user)[0]['nsid']
+end
+
+#フォトセットリストを取得する。
+def getphotosetlist(ac_f,userid)
+  ep = 'http://flickr.com/services/rest/'
+  params = Hash.new
+  params["method"] = "flickr.photosets.getList"
+  params["api_key"] = ac_f['api_key']
+  params["user_id"] = userid
+  params["auth_token"] = ac_f['token']
+  params["api_sig"] = get_api_sig(params,ac_f["secret"])
+
+  agent = Mechanize.new
+  page = agent.post(ep,params)
+  doc = Hpricot(page.body)
+  hash = Hash.new
+  (doc/:photoset).each do |elem|
+    hash[(elem.at(:title)).inner_text]= elem[:id]
+  end
+  return hash
+end
+
+#フォトセットを作成する。
+def makephotoset(ac_f,opts,photoid)
+  ep = 'http://flickr.com/services/rest/'
+  params = Hash.new
+  params["method"] = "flickr.photosets.create"
+  params["title"] = opts.photoset
+  params["primary_photo_id"] = photoid
+  params["api_key"] = ac_f['api_key']
+  params["auth_token"] = ac_f['token']
+  params["api_sig"] = get_api_sig(params,ac_f["secret"])
+
+  agent = Mechanize.new
+  page = agent.post(ep,params)
+end
+
+#フォトセットに写真を追加する。
+def addphotoset(ac_f,opts,photoid,photosetid)
+  ep = 'http://flickr.com/services/rest/'
+  params = Hash.new
+  params["method"] = "flickr.photosets.addPhoto"
+  params["photoset_id"] = photosetid
+  params["photo_id"] = photoid
+  params["api_key"] = ac_f['api_key']
+  params["auth_token"] = ac_f['token']
+  params["api_sig"] = get_api_sig(params,ac_f["secret"])
+
+  agent = Mechanize.new
+  page = agent.post(ep,params)
+end
+
 
 ac_f = account_flickr
 if (ac_f["token"]==nil)
@@ -155,4 +214,23 @@ opts=checkoption
 
 #画像をPostしてフォトIDを取得
 photoid=upload(ac_f,opts)
+
+userid=getuserid(ac_f)
+("ユーザIDが存在取得できなかったため、終了します";exit) if userid == nil
+
+#フォトセット一覧を取得
+p photosetlist = getphotosetlist(ac_f,userid)
+
+#フォトセットの指定があるか？
+if (opts.photoset!=nil)
+  #フォトセットがすでに有る場合、IDを利用し登録。
+  #無ければ新規作成して、Postしたファイルをプライマリにする
+  if( photosetlist.has_key? opts.photoset)
+    photosetid = photosetlist[opts.photoset]
+    addphotoset(ac_f,opts,photoid,photosetid)
+    
+  else
+    makephotoset(ac_f,opts,photoid)
+  end
+end
 
