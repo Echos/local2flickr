e35ac312e07106e11ded2535d1f0a9c216d380a4
えちょ <echos@echo-sp.com>
2010-12-13 00:41:09 +0900

    何故かjpgの設定が消えてた・・・

--- local2flickr.rb
+++ local2flickr.rb
@@ -131,7 +131,7 @@ def upload(ac_f,opts)
   #ディレクトリからファイル一覧を取得する
   if(opts.dir)
     Find.find(opts.dir) do |f|
-      if( /(\.psd$)|(\.PSG$)|(\.jpeg$)|(\.JPEG$)|(\.gif$)|(\.GIF$)|(\.bmp$)|(\.BPM$)|(\.png$)|(\.PNG$)/ =~ f)
+      if( /(\.psd$)|(\.PSG$)|(\.jpeg$)|(\.JPEG$)|(\.jpg$)|(\.JPG$)|(\.gif$)|(\.GIF$)|(\.bmp$)|(\.BPM$)|(\.png$)|(\.PNG$)/ =~ f)
         filelist << f
       end
     end
